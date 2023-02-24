f5a0affcbb13c9b5681d68a8f47ccf4000c9027a
dalveytech-vincent <vincent@dalveytech.com>
2023-02-24 09:43:27 +0000

    Update from https://github.com/dalveytech/depx-view/commit/942f41af39517c50a73032dd473063d9b4819104

--- 563.async.js
+++ 563.async.js
@@ -1,13 +1,1 @@
-(self["webpackChunk"] = self["webpackChunk"] || []).push([[563],{
-
-/***/ 36563:
-/*!************************!*\
-  !*** buffer (ignored) ***!
-  \************************/
-/***/ (function() {
-
-/* (ignored) */
-
-/***/ })
-
-}]);
+(self.webpackChunk=self.webpackChunk||[]).push([[563],{36563:function(){}}]);
